73a8ccc8cbef1d505026c2f0e75b1ee87787db0a
Ziv Bryk <ziv.bryk@gmail.com>
2022-04-04 19:39:39 +0300

    HomePage & HomePageHeader completed

---
+++ src/cmps/home-page-header.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState, useRef, useCallback } from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as TrelloLogoGradient } from "../assets/img/icons/trello-logo-gradient.svg";
+export const HomePageHeader = () => {
+  const [isNavBgVisible, setIsNavBgVisible] = useState(false);
+  const stateRef = useRef(isNavBgVisible);
+
+  const setState = (data) => {
+    stateRef.current = data;
+    setIsNavBgVisible(data);
+  };
+
+  const handleScroll = useCallback(() => {
+    if (window.pageYOffset > 100 && !stateRef.current) setState(true);
+    else if (window.pageYOffset < 100 && stateRef.current) {
+      setState(false);
+    }
+  }, [stateRef]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return function cleanup() {
+      // Side-effect cleanup
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [handleScroll]);
+
+  return (
+    <header
+      className={`home-page-header fixed-top ${
+        isNavBgVisible ? "visibleBg" : ""
+      }`}
+    >
+      <nav className="navbar flex space-between">
+        <Link to="/" className="logo flex">
+          <TrelloLogoGradient />
+          Trellix
+        </Link>
+
+        <div className="login-signup-btns">
+          <Link to="/auth/login" className="btn btn-sm btn-link text-primary">
+            Log in
+          </Link>
+          <Link
+            to="/auth/signup"
+            className="btn btn-sm bg-primary text-white font-bold"
+          >
+            Sign up
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,55 +1,78 @@
+import { HomePageHeader } from "../cmps/home-page-header";
+import { Link } from "react-router-dom";
+
+import heroImageOne from "../assets/img/backgrounds/hero-img-1.png";
+import boardView from "../assets/img/backgrounds/board-view.png";
+
 export const HomePage = () => {
   return (
     <section className="home-page">
-      <h1>Home Page</h1>
+      <HomePageHeader />
+
+      <section className="hero">
+        <div className="container pt-7 pb-6 text-center text-md-left">
+          <div className="row align-items-center">
+            <div className="col-lg-5 offset-lg-1 order-2 hero-image">
+              <img
+                className="img-fluid"
+                src={heroImageOne}
+                alt={"heroImageOne"}
+                width="931"
+                height="1205"
+              />
+            </div>
+
+            <div className="hero-details col-lg-7">
+              <h1>Trello helps teams move work forward.</h1>
+
+              <p>
+                Collaborate, manage projects, and reach new productivity peaks.
+                From high rises to the home office, the way your team works is
+                unique—accomplish it all with Trello.
+              </p>
+
+              <Link to="/board/b101/">
+                <button className="btn btn-prim">Start as guest!</button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="product">
+        <div className="container text-center pt-md-6 pb-6">
+          <div className="row">
+            <div className="col-lg-10 offset-lg-1">
+              <h2>It’s more than work. It’s a way of working together.</h2>
+              <p>
+                Start with a Trello board, lists, and cards. Customize and
+                expand with more features as your teamwork grows. Manage
+                projects, organize tasks, and build team spirit—all in one
+                place.
+              </p>
+              <p>
+                <Link to="/workspace" class="btn btn-outline-prim px-3">
+                  Start doing →
+                </Link>
+              </p>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col mb-4">
+              <img
+                className="img-fluid w-100 d-block"
+                src={boardView}
+                alt={"heroImageOne"}
+                width="931"
+                height="1205"
+              />
+            </div>
+          </div>
+
+          <div className="row"></div>
+        </div>
+      </section>
     </section>
   );
 };
-// import React from "react";
-// import { connect } from "react-redux";
-
-// import logo from "../assets/img/logo.png";
-
-// class _HomePage extends React.Component {
-//     state = {};
-
-//     changeCount = (diff) => {
-//       console.log("Changing count by:", diff);
-//       const action = { type: "CHANGE_COUNT", diff };
-//       this.props.dispatch(action);
-//     };
-
-//   render() {
-//     const { count } = this.props;
-//     return (
-//       <section>
-//         <img src={logo} alt="Logo" style={{ maxWidth: "300px" }} />
-//         <h2>
-//           Count {count}
-//           <button
-//             onClick={() => {
-//               this.changeCount(1);
-//             }}
-//           >
-//             +
-//           </button>
-//           <button
-//             onClick={() => {
-//               this.changeCount(10);
-//             }}
-//           >
-//             +10
-//           </button>
-//         </h2>
-//       </section>
-//     );
-//   }
-// }
-
-// function mapStateToProps(state) {
-//   return {
-//     // count: state.userModule.count,
-//   };
-// }
-
-// export const HomePage = connect(mapStateToProps)(_HomePage);
