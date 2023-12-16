0e20e77f81ea8ba9b7a638025db7319c8668063c
Ziv Bryk <ziv.bryk@zoominfo.com>
2023-12-16 20:58:42 +0200

    added env file & vars for mongo

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,5 +1,4 @@
 var config;
-
 // keys.js - figure out what set of credentials to return
 if (false && process.env.NODE_ENV === 'production') {
   // we are in production - return the prod set of keys
